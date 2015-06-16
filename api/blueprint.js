f72ab91e01992f6785a7d2a38aa8f6a8d6468cf8
Gavin Berkowitz <gberkowitz@logmein.com>
2015-06-16 10:59:02 -0400

    POST new org

--- api/blueprint.js
+++ api/blueprint.js
@@ -17,13 +17,23 @@ var requireAuthorization = function(req,res,next){
 
 router.all('*',requireAuthorization);
 
+//ORGANIZATIONS
 router.route('/organizations')
     .get(function(req,res){
-        manage.get('organizations', {qs: {"accountId": req.query.accountId}}, function(err,data){
+        manage.get('organizations', {qs: {"accountId": req.headers['accountid']}}, function(err,data){
+            res.send(data.body);
+        });
+    })
+    .post(function(req,res){
+        var body = {
+            "accountId": req.headers['accountid'],
+            "name": req.body['name']
+        };
+        manage.post({url: 'organizations', body: body, json:true}, function(err,data){
             console.log(err);
             console.log(data);
+            res.send(data);
         });
-        res.send(200);
     })
 
 
