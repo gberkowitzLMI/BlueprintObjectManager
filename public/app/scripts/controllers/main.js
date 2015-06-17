6d0a07988f3a9541e3d5cac53b5693175fcb87df
Gavin Berkowitz <gberkowitz@logmein.com>
2015-06-17 14:04:18 -0400

    Use querystring params to page

--- public/app/scripts/controllers/main.js
+++ public/app/scripts/controllers/main.js
@@ -1,8 +1,10 @@
 angular.module('DEMO_MODULE').controller('Main', function($scope, Organization){
+    $scope.paging = {};
+    
     var loadOrgs = function(page,pageSize){
-        $scope.page = page;
-        $scope.pageSize = pageSize;
-        Organization.get({}, function(data){
+        $scope.paging.page = page;
+        $scope.paging.pageSize = pageSize;
+        Organization.get({page:$scope.paging.page, pageSize:$scope.paging.pageSize}, function(data){
             $scope.organizations = data.organizations.results;
             $scope.paging = data.organizations.meta;
         });
---
+++ public/app/scripts/directives/pager.js
@@ -0,0 +1,26 @@
+angular.module('DEMO_MODULE').directive('pager', function(){
+    var link = function(scope, elem, attr){
+        scope.prev = function() {
+            if(scope.page > 1){
+                scope.changePage(--scope.page,scope.pageSize);
+            }
+        }
+
+        scope.next = function() {
+            if(scope.count > scope.page * scope.pageSize){
+                scope.changePage(++scope.page,scope.pageSize);
+            }   
+        }
+    };
+
+    return {
+        scope: {
+            count: '=',
+            page: '=',
+            pageSize: '=',
+            changePage: '='
+        },
+        templateUrl: 'app/scripts/directives/pager.html',
+        link: link
+    };
+});
